efdf659ec3f2e972f76565e14d1b9bd02a81efd8
Sergei Mitrofanov <goretz.m@gmail.com>
2020-08-16 18:53:02 +0300

    Add memory data sharer

---
+++ app/src/main/java/com/example/app/interactor/DataReceiver.kt
@@ -0,0 +1,39 @@
+package com.example.app.interactor
+
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
+
+interface DataReceiver<D> {
+    /**
+     * @return {@link Observable<D>} to get shared data. In first call returns last known value if any
+     */
+    fun get(): Observable<D>
+}
+
+
+/**
+ * To share data of type [D] between use cases until process memory is available.
+ *
+ * @param <D> - data type
+</D> */
+abstract class MemoryDataSharer<D> : DataReceiver<D>, Observer<D> {
+    private var subject: BehaviorSubject<D> = BehaviorSubject.create()
+
+    override fun get(): Observable<D> = subject
+
+    override fun onSubscribe(d: Disposable) = subject.onSubscribe(d)
+
+    override fun onNext(d: D) = subject.onNext(d)
+
+    override fun onError(e: Throwable) = with(subject) {
+        subject = BehaviorSubject.create()
+        onError(e)
+    }
+
+    override fun onComplete() = with(subject) {
+        subject = BehaviorSubject.create()
+        onComplete()
+    }
+}
