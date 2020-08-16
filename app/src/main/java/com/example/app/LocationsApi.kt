fb77292449d476b61dcfb5b3ed74b58ac6cff1f3
Sergei Mitrofanov <goretz.m@gmail.com>
2020-08-16 17:54:18 +0300

    Change result to Single since it's a REST

--- app/src/main/java/com/example/app/LocationsApi.kt
+++ app/src/main/java/com/example/app/LocationsApi.kt
@@ -1,10 +1,10 @@
 package com.example.app
 
-import io.reactivex.Observable
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface LocationsApi {
     @GET("home/{brandId}")
-    fun loadHome(@Path("brandId") brandId: String = "0940f8d3-3fbb-432a-8f44-5a467d1da20f"): Observable<LocationsResponse>
+    fun loadHome(@Path("brandId") brandId: String = "0940f8d3-3fbb-432a-8f44-5a467d1da20f"): Single<LocationsResponse>
 }
