a521e31afd0b80c7face9715a72973b02dbf5031
Sergei Mitrofanov <goretz.m@gmail.com>
2020-08-16 19:03:45 +0300

    Implement brand shops use case

--- app/src/main/java/com/example/app/interactor/BrandShopsUseCase.kt
+++ app/src/main/java/com/example/app/interactor/BrandShopsUseCase.kt
@@ -5,20 +5,40 @@ import com.example.app.Shop
 import io.reactivex.Completable
 import io.reactivex.Observer
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 interface BrandShopsUseCase {
     val brandShops: Single<List<Shop>>
     fun selectShopAt(shop: Shop?): Completable
 }
 
-class BrandShopsUseCaseImpl(
-    api: LocationsApi,
-    private val choice: Observer<Shop>
-) : BrandShopsUseCase {
+class BrandShopsUseCaseImpl(api: LocationsApi, private val choice: Observer<Shop>) :
+    BrandShopsUseCase {
 
-    override val brandShops: Single<List<Shop>> = TODO("Not yet implemented")
+    private var shops: List<Shop> = listOf()
 
-    override fun selectShopAt(shop: Shop?) = TODO("Not yet implemented")
+    override val brandShops: Single<List<Shop>> by lazy {
+        api.loadHome()
+            .subscribeOn(Schedulers.io())
+            .map { it.brand?.shops ?: listOf() }
+            .observeOn(Schedulers.single())
+            .doOnSuccess { shops = it }
+            .doOnError { shops = listOf() }
+            .doAfterTerminate { choice.onComplete() }
+            .cache()
+    }
+
+    override fun selectShopAt(shop: Shop?) = Completable.fromCallable {
+        shop
+            ?.let {
+                if (shops.contains(it)) {
+                    choice.onNext(it)
+                } else {
+                    throw IndexOutOfBoundsException("There is no such selection: $shop")
+                }
+            }
+            ?: choice.onComplete()
+    }
 }
 
 class ShopChoiceSharer() : MemoryDataSharer<Shop>()
