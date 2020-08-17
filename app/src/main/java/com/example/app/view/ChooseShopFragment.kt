d13a7856d27722168b4feb1ed1f990f84d337c57
Sergei Mitrofanov <goretz.m@gmail.com>
2020-08-17 18:30:39 +0300

    Add list fragment with animator

---
+++ app/src/main/java/com/example/app/view/ChooseShopFragment.kt
@@ -0,0 +1,113 @@
+package com.example.app.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.app.R
+import com.example.app.presentation.ViewState
+import com.example.app.presentation.chooseshop.ChooseShopListItem
+import com.example.app.presentation.chooseshop.ChooseShopViewActions
+import com.example.app.presentation.chooseshop.ChooseShopViewModel
+import com.example.app.presentation.shoplistselection.ShopListSelectionViewActions
+import com.example.app.presentation.shoplistselection.ShopListSelectionViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_choose_shop.*
+import timber.log.Timber
+
+class ChooseShopFragment : Fragment(), ChooseShopViewActions, ShopListSelectionViewActions {
+
+    lateinit var chooseShopViewModel: ChooseShopViewModel
+
+    lateinit var shopSelectionViewModel: ShopListSelectionViewModel
+
+    private var hideAnimator: HideAnimator? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_choose_shop, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) =
+        super.onActivityCreated(savedInstanceState)
+            .also {
+                retryButton.setOnClickListener {
+                    chooseShopViewModel.reloadShops()
+                }
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = ChooseShopAdapter(listOf()) {}
+                nearby.setOnClickListener {
+                    Timber.d("Nearby clicked!")
+                    Snackbar.make(
+                        it,
+                        "The feature is in progress. Stay tuned",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+                accept.setOnClickListener {
+                    shopSelectionViewModel.accept()
+                }
+                hideAnimator = HideAnimator(
+                    resources.displayMetrics.density * 128,
+                    listOf(accept, gradient)
+                )
+                chooseShopViewModel
+                    .viewStateLiveData
+                    .observe(viewLifecycleOwner,
+                        Observer<ViewState<ChooseShopViewActions>> { it?.apply(this) })
+
+                shopSelectionViewModel
+                    .viewStateLiveData
+                    .observe(viewLifecycleOwner,
+                        Observer<ViewState<ShopListSelectionViewActions>> { it?.apply(this) })
+
+                shopSelectionViewModel
+                    .navigationLiveData
+                    .observe(viewLifecycleOwner,
+                        Observer<String> {
+
+                        })
+            }
+
+    override fun onDestroyView() = super.onDestroyView()
+        .also {
+            hideAnimator = null
+        }
+
+    override fun showShopListPending(show: Boolean) {
+        pending.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun showShopList(show: Boolean) {
+        recyclerView.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun setShopListItems(chooseShopListItems: List<ChooseShopListItem>) {
+        recyclerView.adapter =
+            ChooseShopAdapter(chooseShopListItems, chooseShopViewModel::selectItem)
+    }
+
+    override fun showShopListErrorMessage(show: Boolean) {
+        errorText.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun setShopListErrorMessage(message: String) {
+        errorText.text = message
+    }
+
+    override fun showShopListRetryButton(show: Boolean) {
+        retryButton.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun showSelectedIndex(index: Int?) {
+        (recyclerView.adapter as? ChooseShopAdapter)?.selection = index
+    }
+
+    override fun showAcceptButton(show: Boolean) {
+        hideAnimator?.visible = show
+    }
+}
---
+++ app/src/main/java/com/example/app/view/HideAnimator.kt
@@ -0,0 +1,73 @@
+package com.example.app.view
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.Interpolator
+
+class HideAnimator(private val yDistance: Float, private val views: List<View>) {
+
+    private var currentAnimator: Animator? = null
+
+    var visible = true
+        set(value) {
+            currentAnimator?.cancel()
+            if (field != value) {
+                if (field) {
+                    createAnimator(views, 0f, yDistance, AccelerateInterpolator())
+                } else {
+                    createAnimator(views, 1f, 0f, DecelerateInterpolator())
+                }.animate {
+                    field = value
+                }
+            }
+        }
+
+    private fun AnimatorSet.animate(onResult: () -> Unit) {
+        addListener(
+            object : AnimatorListenerAdapter() {
+                private var cancelled = false
+
+                override fun onAnimationStart(animation: Animator) {
+                    currentAnimator = animation
+                    cancelled = false
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    cancelled = true
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                    if (!cancelled) onResult.invoke()
+                }
+            })
+        start()
+    }
+
+    private fun createAnimator(
+        views: List<View>,
+        alpha: Float,
+        yDistance: Float,
+        interpol: Interpolator
+    ): AnimatorSet {
+        return AnimatorSet()
+            .apply {
+                playTogether(views
+                    .map {
+                        listOf(
+                            ObjectAnimator.ofFloat(it, View.ALPHA, alpha),
+                            ObjectAnimator.ofFloat(it, View.TRANSLATION_Y, yDistance)
+                        )
+                    }
+                    .flatten()
+                )
+                duration = 200
+                interpolator = interpol
+            }
+    }
+}
