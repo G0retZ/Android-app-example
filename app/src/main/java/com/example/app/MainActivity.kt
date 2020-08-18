e5dc21246f34b458b4e8a624e54b99ab5ad49c87
Sergei Mitrofanov <goretz.m@gmail.com>
2020-08-18 10:20:24 +0300

    Add navigator and screens navigation handling

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.app.presentation.chooseshop.CLOSE_CHOOSE_SHOP
+import com.example.app.presentation.selectedshop.CLOSE_SELECTION_DETAILS
+import com.example.app.presentation.shoplistselection.TO_SELECTION_DETAILS
 import com.example.app.view.ChooseShopFragment
+import com.example.app.view.SelectedShopFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,17 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .add(R.id.content, ChooseShopFragment())
                 .commit()
+        }
+    }
+
+    override fun navigate(path: String) {
+        when (path) {
+            TO_SELECTION_DETAILS -> supportFragmentManager.beginTransaction()
+                .replace(R.id.content, SelectedShopFragment())
+                .addToBackStack(null)
+                .commit()
+            CLOSE_CHOOSE_SHOP,
+            CLOSE_SELECTION_DETAILS -> onBackPressed()
         }
     }
 }
---
+++ app/src/main/java/com/example/app/Navigator.kt
@@ -0,0 +1,5 @@
+package com.example.app
+
+interface Navigator {
+    fun navigate(path: String)
+}
